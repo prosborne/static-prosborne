3cf997b3f059a9155092cf04acac1a5a91b23de6
Peter Riley Osborne <posborne@cbtnuggets.com>
2019-03-11 10:28:34 -0700

    Updated files

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
-const Index = () => (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  )
-  export default Index
+import { Component } from "react";
+import Template from "../components/Template";
+import Meta from "../components/Meta";
+export default class Index extends Component {
+    render() {
+        return (
+            <Template>
+                <Meta
+                    title={`Remote Job Lists`}
+                    description={`Some sample description`}
+                />
+                <p>Hello Next.js</p>
+            </Template>
+        );
+    }
+}
